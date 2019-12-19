f0ef9d3a91a0348f0338e65d2106796386bf086f
queeksm <queeksm@gmail.com>
2019-12-19 10:27:32 -0500

    Additional game logic and Board class created

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class Cell
-  attr_accessor :sign
-  def initialize(sign = nil)
-    @sign = sign
+  attr_accessor :value
+  def initialize(value = "")
+    @value = value
   end
 end
 
@@ -13,6 +13,71 @@ class Player
     @color = color
   end
 end
+
+class Board 
+
+  attr_reader :grid  
+  def initialize(grid = empty_grid)
+    @grid = grid
+  end 
+
+  def empty_grid
+    Array.new(3) { Array.new(3) { Cell.new } }
+  end
+
+  public
+
+  def get_cell(x,y)
+    grid[x][y]
+  end
+
+  public
+
+  def set_cell(x,y,value)
+    get_cell(x,y).value = value
+  end
+
+
+  private
+  
+  def human_input(number)
+    case number
+    when number = 1
+      coordinates = [0,0]     
+    when number = 2
+      coordinates = [0,1]     
+    when number = 3
+      coordinates = [0,2]
+    when number = 4
+      coordinates = [1,0]
+    when number = 5
+      coordinates = [1,1]
+    when number = 6
+      coordinates = [1,2]
+    when number = 7
+      coordinates = [2,0]
+    when number = 8
+      coordinates = [2,1]
+    when number = 9
+      coordinates = [2,2]
+    else
+      false      
+    end
+  end 
+
+  def game_over
+    return :winner if winner?
+    return :draw if draw?
+    false
+  end
+
+
+  def winner
+    
+  end
+end
+
+
 
 def rules_question(rules_answer)
   if rules_answer.match(/y/)
